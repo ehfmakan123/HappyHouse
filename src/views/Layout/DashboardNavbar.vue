bb3cb823e510c1d412b90adfe8d0661d5d50ca36
정해윤 <godbs98@naver.com>
2021-11-25 00:38:36 +0900

    email발송 기능

--- src/views/Layout/DashboardNavbar.vue
+++ src/views/Layout/DashboardNavbar.vue
@@ -2,50 +2,68 @@
   <base-nav
     container-classes="container-fluid"
     class="navbar-top navbar-expand"
-    :class="{'navbar-dark': type === 'default'}"
+    :class="{ 'navbar-dark': type === 'default' }"
   >
     <!-- <a href="#" aria-current="page" class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block active router-link-active"> {{$route.name}} </a> -->
     <!-- Navbar links -->
     <b-navbar-nav class="align-items-center ml-md-auto">
       <!-- This item dont have <b-nav-item> because item have data-action/data-target on tag <a>, wich we cant add -->
       <li class="nav-item d-sm-none">
-        <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
+        <a
+          class="nav-link"
+          href="#"
+          data-action="search-show"
+          data-target="#navbar-search-main"
+        >
           <i class="ni ni-zoom-split-in"></i>
         </a>
       </li>
     </b-navbar-nav>
     <b-navbar-nav class="align-items-center ml-auto ml-md-0">
-        <b-form class="navbar-search form-inline mr-sm-3"
-            :class="{'navbar-search-dark': type === 'default', 'navbar-search-light': type === 'light'}"
-            id="navbar-search-main">
+      <b-form
+        class="navbar-search form-inline mr-sm-3"
+        :class="{
+          'navbar-search-dark': type === 'default',
+          'navbar-search-light': type === 'light',
+        }"
+        id="navbar-search-main"
+      >
         <b-form-group class="mb-0">
           <b-input-group class="input-group-alternative input-group-merge">
             <b-form-input placeholder="Search" type="text"> </b-form-input>
 
             <div class="input-group-append">
-              <span class="input-group-text"><i class="fas fa-search"></i></span>
+              <span class="input-group-text"
+                ><i class="fas fa-search"></i
+              ></span>
             </div>
           </b-input-group>
         </b-form-group>
       </b-form>
-      <base-dropdown menu-on-right
-                     class="nav-item"
-                     tag="li"
-                     title-tag="a"
-                     title-classes="nav-link pr-0">
+      <base-dropdown
+        menu-on-right
+        class="nav-item"
+        tag="li"
+        title-tag="a"
+        title-classes="nav-link pr-0"
+      >
         <a href="#" class="nav-link pr-0" @click.prevent slot="title-container">
           <b-media no-body class="align-items-center">
-                  <span class="avatar avatar-sm rounded-circle">
-                    <img alt="Image placeholder" src="img/theme/team-4.jpg">
-                  </span>
-            <b-media-body class="ml-2 d-none d-lg-block" v-if="userInfo != null">
-              <span class="mb-0 text-sm  font-weight-bold">{{userInfo.userid}}님 환영합니다.</span>
+            <span class="avatar avatar-sm rounded-circle">
+              <img alt="Image placeholder" src="img/theme/team-4.jpg" />
+            </span>
+            <b-media-body
+              class="ml-2 d-none d-lg-block"
+              v-if="userInfo != null"
+            >
+              <span class="mb-0 text-sm font-weight-bold"
+                >{{ userInfo.userid }}님 환영합니다.&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
             </b-media-body>
           </b-media>
         </a>
 
         <template>
-
           <b-dropdown-header class="noti-title">
             <h6 class="text-overflow m-0">Welcome!</h6>
           </b-dropdown-header>
@@ -66,19 +84,19 @@
             <span>Support</span>
           </b-dropdown-item>
           <div class="dropdown-divider"></div>
-          <b-dropdown-item href="#!">
+          <b-dropdown-item href="#" @click="setLogout" > 
+            <!-- dashbouardnavbar에서 setLogout 함수 불러와야함-->
             <i class="ni ni-user-run"></i>
             <span>Logout</span>
           </b-dropdown-item>
-
         </template>
       </base-dropdown>
     </b-navbar-nav>
   </base-nav>
 </template>
 <script>
-import { CollapseTransition } from 'vue2-transitions';
-import { BaseNav, Modal } from '@/components';
+import { CollapseTransition } from "vue2-transitions";
+import { BaseNav, Modal } from "@/components";
 import { mapState } from "vuex";
 const memberStore = "memberStore";
 
@@ -86,28 +104,29 @@ export default {
   components: {
     CollapseTransition,
     BaseNav,
-    Modal
+    Modal,
   },
   props: {
     type: {
       type: String,
-      default: 'default', // default|light
-      description: 'Look of the dashboard navbar. Default (Green) or light (gray)'
-    }
+      default: "default", // default|light
+      description:
+        "Look of the dashboard navbar. Default (Green) or light (gray)",
+    },
   },
   computed: {
     ...mapState(memberStore, ["userInfo"]),
     routeName() {
       const { name } = this.$route;
       return this.capitalizeFirstLetter(name);
-    }
+    },
   },
   data() {
     return {
       activeNotifications: false,
       showMenu: false,
       searchModalVisible: false,
-      searchQuery: ''
+      searchQuery: "",
     };
   },
   methods: {
@@ -119,7 +138,7 @@ export default {
     },
     closeDropDown() {
       this.activeNotifications = false;
-    }
-  }
+    },
+  },
 };
 </script>
