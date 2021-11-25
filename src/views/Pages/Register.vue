1beb39e0bedb77fafe3717bd6975ff671221e5e6
김은지 <a_sldkfj@naver.com>
2021-11-26 07:39:46 +0900

    commmmmmmmmmit

--- src/views/Pages/Register.vue
+++ src/views/Pages/Register.vue
@@ -35,25 +35,6 @@
       <b-row class="justify-content-center">
         <b-col lg="6" md="8">
           <b-card no-body class="bg-secondary border-0">
-            <!-- <b-card-header class="bg-transparent pb-5">
-              <div class="text-muted text-center mt-2 mb-4">
-                <small>Sign up with</small>
-              </div>
-              <div class="text-center">
-                <a href="#" class="btn btn-neutral btn-icon mr-4">
-                  <span class="btn-inner--icon"
-                    ><img src="img/icons/common/github.svg"
-                  /></span>
-                  <span class="btn-inner--text">Github</span>
-                </a>
-                <a href="#" class="btn btn-neutral btn-icon">
-                  <span class="btn-inner--icon"
-                    ><img src="img/icons/common/google.svg"
-                  /></span>
-                  <span class="btn-inner--text">Google</span>
-                </a>
-              </div>
-            </b-card-header> -->
             <b-card-body class="px-lg-5 py-lg-5">
               <div class="text-center text-muted mb-4">
                 <large>Sign Up</large>
