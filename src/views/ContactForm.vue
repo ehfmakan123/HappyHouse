9ddfff680fbe4bbee0366c2bae91aafe59b653cd
정해윤 <godbs98@naver.com>
2021-11-25 20:52:10 +0900

    contact form 디자인 및 메일 송신 기능 세부 수정

--- src/views/ContactForm.vue
+++ src/views/ContactForm.vue
@@ -1,34 +1,89 @@
 <template>
-  <div class="container" align="center">
-    <br /><br />
-    <br /><br />
-    Send mail
-    <br />
-    <b-card class="t1">
-      <form ref="form" @submit.prevent="sendEmail" class="bg">
-        <label>ID</label>
-        <input
-          type="text"
-          v-model="name"
-          name="name"
-          placeholder="Receiving ID"
-        />
-        <label>Email</label>
-        <input type="email" v-model="email" name="email" placeholder="Email" />
-        <label>Message</label>
-        <textarea
-          name="message"
-          v-model="message"
-          cols="30"
-          rows="5"
-          placeholder="Message"
-        >
-        </textarea>
+  <div i1="app">
+    <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
+      <b-container class="container">
+        <div class="header-body text-center mb-7">
+          <b-row class="justify-content-center">
+            <b-col xl="5" lg="6" md="8" class="px-5">
+              <h1 class="text-white">Send mail (Admin)</h1>
+              <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
+                free.</p> -->
+            </b-col>
+          </b-row>
+        </div>
+      </b-container>
 
-        <input type="submit" value="Send" />
-      </form>
-    </b-card>
-    <br /><br /><br /><br />
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
+    </div>
+
+    <b-container class="mt--8 pb-5">
+      <!-- Table -->
+      <b-row class="justify-content-center">
+        <b-col lg="6" md="8">
+          <b-card no-body class="bg-primary border-3">
+          <div class="text-center"></div>
+            <div class="container" align="center">
+              <br />
+              <div class="text-white">
+              Send mail
+              </div>
+              <br />
+              <b-card class="t1">
+                <b-form ref="form" @submit.prevent="sendEmail" class="bg">
+                  <label>ID</label>
+                  <input
+                    class="mb-3"
+                    prepend-icon="ni ni-circle-08"
+                    type="text"
+                    v-model="name"
+                    name="name"
+                    placeholder="Receiving ID"
+                  />
+                  <label>Email</label>
+                  <input
+                    class="mb-3"
+                    prepend-icon="ni ni-email-83"
+                    type="email"
+                    v-model="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  <label>Message</label>
+                  <textarea
+                    name="message"
+                    v-model="message"
+                    cols="30"
+                    rows="5"
+                    placeholder="Message"
+                  >
+                  </textarea>
+                  <br /><br />
+                   <b-button type="submit" variant="info" class="mt-4"
+                      >Send</b-button
+                    >
+                  <br />
+                </b-form>
+              </b-card>
+            </div>
+            <br /><br /><br />
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -47,14 +102,16 @@ export default {
 
   methods: {
     sendEmail(e) {
+      if (confirm("메일을 발송 하시겠습니까?")) {
       try {
         emailjs.sendForm(
           "HappyHouse",
           "template_92xfmzn",
           e.target,
-          "user_xXq7X8YXK6mZeeYLZsN6n",
+          "user_xXq7X8YXK6mZeeYLZsN6n"
         );
         console.log(e.target);
+        alert("메일이 발송 되었습니다.")
       } catch (error) {
         console.log({ error });
       }
@@ -62,57 +119,12 @@ export default {
       this.name = "";
       this.email = "";
       this.message = "";
+      }
     },
-  },
+  }
 };
 </script>
 
 <style scope="this api replaced by slot-scope in 2.5.0+">
-* {
-  box-sizing: border-box;
-}
-.container {
-  background-color: darkseagreen;
-}
 
-.t1 {
-  display: auto;
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-.bg {
-  display: auto;
-  margin: auto;
-  text-align: center;
-  border-radius: 5px;
-  background-color: #f2f2f2;
-  padding: 20px;
-  width: 50%;
-}
-label {
-  float: left;
-}
-input[type="text"],
-[type="email"],
-textarea {
-  width: 100%;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  margin-top: 6px;
-  margin-bottom: 16px;
-  resize: vertical;
-}
-input[type="submit"] {
-  background-color: #4caf50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-input[type="submit"]:hover {
-  background-color: #45a049;
-}
 </style>
