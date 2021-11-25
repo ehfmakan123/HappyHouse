93d176cc2a0e40aa1a20c9c4d512308f9e5430cf
정해윤 <godbs98@naver.com>
2021-11-25 23:24:35 +0900

    edit profile 수정

--- src/views/Pages/UserProfile/EditProfileForm.vue
+++ src/views/Pages/UserProfile/EditProfileForm.vue
@@ -8,32 +8,80 @@
 
     <b-form @submit.prevent="updateProfile">
       <h6 class="heading-small text-muted mb-4">User information</h6>
+      <br />
+      <!-- 수정 모드가 아닐때 (ismodify = false 모드 )-->
+      
+      <div class="pl-lg-4"> 
+        <b-row>
+          <b-col lg="6" > <!-- 모드 상관없이 수정 X -->
+            <base-input type="text" label="UserID" 
+              ><b-iocn class="ni ni-circle-08" />&nbsp;&nbsp;&nbsp;
+              {{ userInfo.userid }}
+            </base-input>
+          </b-col>
+          <b-col lg="6" v-if="!ismodify">  <!-- (ismodify = false 모드 email 보여줌 )-->
+            <base-input type="text" label="Email"
+              ><b-iocn class="ni ni-email-83" />&nbsp;&nbsp;&nbsp;
+              {{ userInfo.email }}
+            </base-input>
+          </b-col>
 
-      <div class="pl-lg-4">
-        <b-row>
-          <b-col lg="6">
-            <base-input type="text" label="UserID" aria-disabled
-              ><b-iocn class="ni ni-circle-08" />&nbsp;&nbsp;&nbsp; {{ userInfo.userid }}
+          <b-col lg="6" v-if="ismodify"> <!-- 수정 모드일때 (ismodify = true 모드 )-->
+            <base-input
+              type="Email"
+              label="Email"
+              placeholder="example@email.com"
+              v-model="user.email"
+            >
             </base-input>
           </b-col>
-          <b-col lg="6">
-            <base-input type="text" label="Email" aria-disabled
-              ><b-iocn class="ni ni-email-83" />&nbsp;&nbsp;&nbsp; {{ userInfo.email }}
+
+        </b-row>
+
+        <b-row>
+          <b-col lg="6" v-if="!ismodify"> <!-- (ismodify = false 모드 username 보여줌)-->
+            <base-input type="text" label="UserName"
+              ><b-iocn class="ni ni-hat-3" />&nbsp;&nbsp;&nbsp;
+              {{ userInfo.username }}
             </base-input>
+          </b-col>
+
+          <b-col lg="6" v-if="ismodify"> <!-- 수정 모드일때 (ismodify = true 모드 )-->
+            <base-input
+              type="text"
+              label="UserName"
+              placeholder="UserName"
+              v-model="user.username"
+            >
+            </base-input>
+          </b-col>
+
+          <b-col lg="6"> <!-- 모드 상관없이 수정 X -->
+            <base-input type="text" label="Join Date"
+              ><b-iocn class="ni ni-time-alarm" />&nbsp;&nbsp;&nbsp;
+              {{ userInfo.joindate }}
+            </base-input>
+          </b-col>
+
+        </b-row>
+
+        <b-row> <!-- 수정 모드일때 (ismodify = true 모드 )-->
+          <b-col lg="6" v-if="ismodify">
+            <base-input
+              type="text"
+              label="PassWord"
+              placeholder="PassWord"
+              v-model="userInfo.userpwd"
+            >
+            </base-input>
+          </b-col>
+
+          <b-col lg="6" >
           </b-col>
         </b-row>
-        <b-row>
-          <b-col lg="6">
-            <base-input type="text" label="UserName" aria-disabled
-              ><b-iocn class="ni ni-hat-3" />&nbsp;&nbsp;&nbsp; {{ userInfo.username }}
-            </base-input>
-          </b-col>
-         <b-col lg="6">
-            <base-input type="text" label="Join Date" aria-disabled
-              ><b-iocn class="ni ni-time-alarm" />&nbsp;&nbsp;&nbsp; {{ userInfo.joindate }}
-            </base-input>
-          </b-col>
-        </b-row>
+        <br/>
+      <button class="btn btn-info">Edit profile</button> 
+      <!-- <button @click="modifyform" class="btn btn-info">Edit profile</button>  클릭하면 modifyform 함수를 사용해서 idmodify를 true로  -->
       </div>
       <hr class="my-4" />
     </b-form>
@@ -50,8 +98,13 @@ export default {
         userid: "",
         email: "",
         username: "",
+        ismodify : true,
+        userpwd:"",
       },
     };
+  },
+  mounted:{
+    ismodify :true 
   },
   computed: {
     ...mapState(memberStore, ["userInfo"]),
@@ -60,6 +113,11 @@ export default {
     updateProfile() {
       alert("Your data: " + JSON.stringify(this.user));
     },
+    modifyform(){ //?????
+      ismodify = true;
+      console.log(this.ismodify);
+      this.$router.go();
+    }
   },
 };
 </script>
