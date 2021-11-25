dcddd5c4bb5165012bc2c62e7e53a6bf42a166b0
정해윤 <godbs98@naver.com>
2021-11-26 03:34:32 +0900

    게시판 수정

--- src/views/RegularTables.vue
+++ src/views/RegularTables.vue
@@ -1,53 +1,80 @@
 <template>
   <div id="app">
-    <div class="bg-image pb-6 pb-8 pt-5 pt-md-8">
-      <!-- Card stats -->
+    <div class="header bg-gradient-warning py-7 py-lg-8 pt-lg-9">
+      <b-container>
+        <div class="header-body text-center mb-7">
+          <b-row class="justify-content-center">
+            <b-col xl="5" lg="6" md="8" class="px-5">
+              <h1 class="text-white"> 게 시 판 </h1>
+              <!-- <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for
+                free.</p> -->
+            </b-col>
+          </b-row>
+        </div>
+      </b-container>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon
+            class="fill-default"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
     </div>
-    <b-container fluid class="mt--7">
-      <b-row>
-        <b-col>
-          <stats-card sub-title="게시판"
-                      icon="ni ni-bullet-list-67"
-                      class="mb-4">
-          </stats-card>
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <light-table/>
-        </b-col>
-      </b-row>
-      <!-- <div class="mt-5"></div>
+    <div class="bg2">
+      <b-container fluid class="mt--7">
+        <b-row> </b-row>
+        <b-row>
+          <b-col>
+            <light-table />
+            <br/><br/><br/><br/>
+          </b-col>
+        </b-row>
+        <!-- <div class="mt-5"></div>
       <dark-table></dark-table> -->
-    </b-container>
+      </b-container>
+    </div>
   </div>
 </template>
 <script>
-  import { Dropdown, DropdownItem, DropdownMenu, Table, TableColumn } from 'element-ui';
-  import LightTable from "./Tables/RegularTables/LightTable";
-  // import DarkTable from "./Tables/RegularTables/DarkTable";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  Table,
+  TableColumn,
+} from "element-ui";
+import LightTable from "./Tables/RegularTables/LightTable";
+// import DarkTable from "./Tables/RegularTables/DarkTable";
 
-  export default {
-    components: {
-      LightTable,
-      // DarkTable,
-      [Dropdown.name]: Dropdown,
-      [DropdownItem.name]: DropdownItem,
-      [DropdownMenu.name]: DropdownMenu,
-      [Table.name]: Table,
-      [TableColumn.name]: TableColumn
-    },
-    data() {
-      return {
-      };
-    }
-  };
+export default {
+  components: {
+    LightTable,
+    // DarkTable,
+    [Dropdown.name]: Dropdown,
+    [DropdownItem.name]: DropdownItem,
+    [DropdownMenu.name]: DropdownMenu,
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 <style scoped>
 .el-table tr :hover {
-  cursor:pointer;
+  cursor: pointer;
   /* background-color: lightblue; */
 }
+/* 
 .bg-image {
   width: 100%;
   height: 100%;
@@ -67,9 +94,11 @@
   top: 0;
   left: 0;
   z-index: -1;
-  /* min-height: 500px; */
-  background-size: cover; 
+  background-size: cover;
   background-position: center top;
-}
+} */
 
+.bg2{
+  background-color: rgb(15, 11, 41);
+}
 </style>
